d5b1f4b9e9f4c6771a64316a954d25c58f7ddc8f
seakjh <skykjh0513@gmail.com>
2020-03-17 15:11:50 +0900

    DataBinding 연습

--- app/src/main/java/com/example/a20200317_02_java_first/MainActivity.java
+++ app/src/main/java/com/example/a20200317_02_java_first/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.a20200317_02_java_first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.a20200317_02_java_first.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = binding.inputEdt.getText().toString();
+
+                binding.resultTxt.setText(input);
+                binding.inputEdt.setText("");
+            }
+        });
+
     }
 }
